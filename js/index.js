6f8d7f157dd6d688e01669acfa48ed233bd6655e
verygois <sasajimakazuma@gmail.com>
2024-04-25 18:22:53 +0900

    index

--- js/index.js
+++ js/index.js
@@ -35,9 +35,9 @@ window.addEventListener("load", () => {
     let month = document.querySelector('#phase h1 time').textContent;
     let lunarPhase = Number(month).toFixed(0)
 
-    if (!localStorage.getItem('yourInfo')) {
-        submitStars(`29d12h44m3s/${lunarPhase}.csv`)
-    } else {
+    submitStars(`29d12h44m3s/${lunarPhase}.csv`)
+
+    if (localStorage.getItem('yourInfo')) {
         for (let m = 0; m < 30; m++) {
             const button = document.createElement('button')
             button.setAttribute('type', 'button')
@@ -52,35 +52,35 @@ window.addEventListener("load", () => {
             }, false)
         }
 
-        if (localStorage.getItem('emoji')) {
-            const main = document.querySelector('main')
-            const emojiJSON = JSON.parse(localStorage.getItem('emoji'))
-            for (let i = 0; i < emojiJSON.length; i++) {
-                let emoji = emojiJSON[i].emojiValue;
-                let size = emojiJSON[i].fontSize;
-                let lunar = emojiJSON[i].lunarPhase;
+        if (location.search) {
+            if (localStorage.getItem('emoji')) {
+                const main = document.querySelector('main')
+                const emojiJSON = JSON.parse(localStorage.getItem('emoji'))
+                for (let i = 0; i < emojiJSON.length; i++) {
+                    let emoji = emojiJSON[i].emojiValue;
+                    let size = emojiJSON[i].fontSize;
+                    let lunar = emojiJSON[i].lunarPhase;
 
-                if (Number(lunar) == lunarPhase) {
-                    const star = document.createElement('code')
-                    star.textContent = emoji;
-                    star.style.fontSize = size;
-                    star.style.top = getRandomInt(0, 100) + "%";
-                    star.style.left = getRandomInt(0, 100) + "%";
-                    main.appendChild(star)
+                    if (Number(lunar) == lunarPhase) {
+                        const star = document.createElement('code')
+                        star.textContent = emoji;
+                        star.style.fontSize = size;
+                        star.style.top = getRandomInt(0, 100) + "%";
+                        star.style.left = getRandomInt(0, 100) + "%";
+                        main.appendChild(star)
 
-                    star.addEventListener('click', function () {
-                        let result = window.confirm('この絵文字をコレクションから削除します。 \r\n Remove This from Your Collection.')
-                        if (result) {
-                            emojiJSON.splice(i, 1)
-                            localStorage.setItem('emoji', JSON.stringify(emojiJSON))
-                            location.reload()
-                        }
-                    }, false)
+                        star.addEventListener('click', function () {
+                            let result = window.confirm('この絵文字をコレクションから削除します。 \r\n Remove This from Your Collection.')
+                            if (result) {
+                                emojiJSON.splice(i, 1)
+                                localStorage.setItem('emoji', JSON.stringify(emojiJSON))
+                                location.reload()
+                            }
+                        }, false)
+                    }
                 }
             }
-        }
-
-        if (!location.search) {
+        } else {
             const submitForm = document.querySelector('#submit')
             submitForm.addEventListener('submit', (e) => {
                 e.preventDefault()
@@ -96,7 +96,7 @@ window.addEventListener("load", () => {
                     }
                 }
 
-                let fontSize
+                let fontSize;
                 for (let i = 0; i < toAll.length; i++) {
                     if (toAll[i].checked) {
                         fontSize = toAll[i].value;
